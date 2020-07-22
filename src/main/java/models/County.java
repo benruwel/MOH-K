fb596883022954965f7a3b63f6f2a62e16393599
Gideon Okuro <gideonollonde@gmail.com>
2020-07-22 10:25:14 +0300

    instantiate first instance of county

---
+++ src/main/java/models/County.java
@@ -0,0 +1,7 @@
+package models;
+
+public class County {
+    public County(String county, int totalcases, int newcases, int totaldeaths, int newdeaths, int totalrecoveries, int newrecoveries, int activecases) {
+
+    }
+}
---
+++ src/test/java/models/CountyTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountyTest {
+    @Test
+    public void countyInstantiatesCorrectly() {
+        County county = new County("Nairobi", 13771, 540, 238, 68, 5616, 18, 7917);
+        assertEquals(true, county instanceof County);
+    }
+
+}
