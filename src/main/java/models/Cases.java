fba4b9f2f7d4d1e180045cb3fc9b5bf061e956d3
Gideon Okuro <gideonollonde@gmail.com>
2020-07-22 13:42:15 +0300

    add getter methods

--- src/main/java/models/Cases.java
+++ src/main/java/models/Cases.java
@@ -1,4 +1,25 @@
 package models;
 
 public class Cases {
+    private int caseid;
+    private int countyid;
+    private String countystatus;
+    private int id;
+
+    public Cases(int caseid, int countyid, String countystatus) {
+        this.caseid = caseid;
+        this.countyid = countyid;
+    }
+
+    public int getCaseId() {
+        return caseid;
+    }
+
+    public int getCountyId() {
+        return countyid;
+    }
+
+    public String getCountyStatus() {
+        return countystatus;
+    }
 }
--- src/test/java/models/CasesTest.java
+++ src/test/java/models/CasesTest.java
@@ -1,5 +1,31 @@
 package models;
 
-public class CasesTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class CasesTest {
+    @Test
+    public void casesInstantiatesCorrectly() {
+        Cases cases = new Cases (1, 47, "Lockdown");
+        assertEquals(true, cases instanceof Cases);
+    }
+
+    @Test
+    public void getCaseId() {
+        Cases cases = new Cases (1, 47, "Lockdown");
+        assertEquals(1, cases.getCaseId());
+    }
+
+    @Test
+    public void getCountyId() {
+        Cases cases = new Cases (1, 47, "Lockdown");
+        assertEquals(47, cases.getCountyId());
+    }
+
+    @Test
+    public void getCountyStatus() {
+        Cases cases = new Cases (1, 47, "Lockdown");
+        assertEquals("Lockdown", cases.getCountyStatus());
+    }
 }
