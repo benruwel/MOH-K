53f33997c2bba1695d0aefda05741a8e5d74a749
Linda Tonui <tonuilinda@gmail.com>
2020-07-23 09:09:36 +0300

    Add routes

--- create.sql
+++ create.sql
@@ -1,16 +1,13 @@
-CREATE DATABASE healthApp;
-\c healthApp
+CREATE DATABASE healthapp;
+\c healthapp
 
-CREATE TABLE IF NOT EXISTS counties{
-id SERIAL PRIMARY KEY int,
+CREATE TABLE IF NOT EXISTS counties(
+id SERIAL PRIMARY KEY,
 countyName VARCHAR,
-countyPopulation int,
-totalCases int,
-
-};
-
-CREATE TABLE IF NOT EXISTS cases{
-id SERIAL PRIMARY KEY  int,
-county_id int,
-caseStatus VARCHAR
-};
+countyCode INT,
+countyPopulation INT,
+totalCases INT,
+activeCases INT,
+recoveredCases INT,
+deathCases INT
+);
